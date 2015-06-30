10cb05305f2244f94337b072d7056cc7e4e3c7ce
cesine <cesine@yahoo.com>
2015-06-29 23:20:33 -0400

    added test for if pdf viewer is available

--- mobile/src/main/java/com/egnyte/intent/test/EgnyteIntentTest.java
+++ mobile/src/main/java/com/egnyte/intent/test/EgnyteIntentTest.java
@@ -6,6 +6,8 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import com.egnyte.intent.LoginActivity;
 
@@ -15,6 +17,7 @@ import java.util.List;
  * http://rexstjohn.com/unit-testing-with-android-studio/
  */
 public class EgnyteIntentTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+    static String TAG = "EgnyteIntentTest";
 
     public EgnyteIntentTest() {
         super(LoginActivity.class);
@@ -31,13 +34,21 @@ public class EgnyteIntentTest extends ActivityInstrumentationTestCase2<LoginActi
         assertEquals(intentWhichShouldNotWork, false);
     }
 
+    public void testIsViewerAvailable() throws Exception {
+        Intent intent = isViewerAvailable(getActivity(), "/sdcard/Download/sample.pdf");
+        assertNotNull(intent);
+
+        intent = isViewerAvailable(getActivity(), "file:///android_asset/sample.pdf");
+        assertNotNull(intent);
+    }
+
     public void testIsEgnyteSchemaAvailable() throws Exception {
         boolean isEgnyteSchemaAvailable = isIntentAvailable(getActivity(), Intent.ACTION_VIEW, "egnyte:///openFolder/?path=/Shared/Documents/");
         assertEquals(isEgnyteSchemaAvailable, true);
     }
 
     public void testCanEgnyteReplyToOpenIntentsPickFile() throws Exception {
-        Intent intent = new Intent( "org.openintents.action.PICK_FILE");
+        Intent intent = new Intent("org.openintents.action.PICK_FILE");
         intent.setDataAndType(Uri.parse("file:///android_asset/sample.pdf"), "application/pdf");
 
         PackageManager packageManager = getActivity().getPackageManager();
@@ -51,5 +62,31 @@ public class EgnyteIntentTest extends ActivityInstrumentationTestCase2<LoginActi
         PackageManager packageManager = context.getPackageManager();
         List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
         return list.size() > 0;
+    }
+
+    public static Intent isViewerAvailable(Context context, String uri) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        String fileType = MimeTypeMap.getFileExtensionFromUrl(uri);
+        if (fileType == null || "".equals(fileType)) {
+            fileType = uri.substring(uri.lastIndexOf(".") + 1);
+        }
+        String mimeType = mimeTypeMap.getMimeTypeFromExtension(fileType);
+
+        if (uri.indexOf("sdcard") > 5) {
+            intent.setDataAndType(Uri.parse(uri), mimeType);
+        } else {
+            //enables webbrowser to open as before
+            intent.setData(Uri.parse(uri));
+        }
+
+        PackageManager packageManager = context.getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, 0);
+        if (list.size() > 0) {
+            Log.d(TAG, "Found " + list.size() + " activities which can handle this mime type " + mimeType);
+            return intent;
+        } else {
+            return null;
+        }
     }
 }
